f99291ab814e9f3b5accb8e9f133f95d091a9cb7
Faraz <faraz.fatahnaie@gmail.com>
2023-10-20 03:52:35 +0330

    parse_data() is for reading dataset - OptimizerFactory is for generating optimizer based on CONFIG.json for autoencoder and classifier training.

---
+++ utils.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+from keras.optimizers import Adam, SGD, RMSprop
+
+
+def parse_data(df, dataset_name: str, classification_mode: str, mode: str = 'np'):
+    classes = []
+    if classification_mode == 'binary':
+        classes = df.columns[-2:]
+    elif classification_mode == 'multi':
+        if dataset_name in ['NSL_KDD', 'KDD_CUP99']:
+            classes = df.columns[-5:]
+        elif dataset_name == 'UNSW_NB15':
+            classes = df.columns[-10:]
+        elif dataset_name == 'CICIDS':
+            classes = df.columns[-15:]
+
+    assert classes is not None, 'Something Wrong!!\nno class columns could be extracted from dataframe'
+    glob_cl = set(range(len(df.columns)))
+    cl_idx = set([df.columns.get_loc(c) for c in list(classes)])
+    target_feature_idx = list(glob_cl.difference(cl_idx))
+    cl_idx = list(cl_idx)
+    dt = df.iloc[:, target_feature_idx]
+    lb = df.iloc[:, cl_idx]
+    assert len(dt) == len(lb), 'Something Wrong!!\nnumber of data is not equal to labels'
+    if mode == 'np':
+        return dt.to_numpy(), lb.to_numpy()
+    elif mode == 'df':
+        return dt, lb
+
+
+class OptimizerFactory:
+    def __init__(self,
+                 opt: str = 'adam',
+                 lr_schedule: bool = True,
+                 len_dataset: int = 494021,
+                 epochs: int = 50,
+                 batch_size: int = 100,
+                 init_lr: float = 0.1,
+                 final_lr: float = 0.00001):
+        self.opt = opt
+        self.lr_schedule = lr_schedule
+        self.len_dataset = len_dataset
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.init_lr = init_lr
+        self.final_lr = final_lr
+
+    def lr_scheduler(self):
+        pretraining_learning_rate_decay_factor = (self.final_lr / self.init_lr) ** (1 / self.epochs)
+        pretraining_steps_per_epoch = int(self.len_dataset / self.batch_size)
+        lr_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=self.init_lr,
+            decay_steps=pretraining_steps_per_epoch,
+            decay_rate=pretraining_learning_rate_decay_factor,
+            staircase=True)
+        return lr_scheduler
+
+    def get_opt(self):
+        if self.opt == 'adam':
+            if self.lr_schedule:
+                return Adam(self.lr_scheduler())
+            else:
+                return Adam(learning_rate=self.init_lr)
+
+        elif self.opt == 'sgd':
+            if self.lr_schedule:
+                return SGD(self.lr_scheduler())
+            else:
+                return SGD(learning_rate=5, decay=0.5, momentum=.85, nesterov=True)
+                # return SGD(learning_rate=.1, decay=0.001, momentum=.95, nesterov=True)
+
+        elif self.opt == 'rmsprop':
+            if self.lr_schedule:
+                return RMSprop(self.lr_schedule)
+            else:
+                return RMSprop(learning_rate=self.init_lr)
+
+
